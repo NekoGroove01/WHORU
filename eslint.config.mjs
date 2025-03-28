4eb252006c3d6c888ebfbc8c4b61e3733e1d6035
noobcpuls <iop789007@gmail.com>
2025-03-28 17:39:25 +0900

    Chore: Update Rules - no unused vars to warn

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,24 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+	...compat.extends("next/core-web-vitals", "next/typescript"),
+	{
+		files: ["**/*.js", "**/*.jsx"],
+		rules: {
+			"no-unused-vars": "warn",
+		},
+	},
+	// TypeScript 파일에 대한 설정
+	{
+		files: ["**/*.ts", "**/*.tsx"],
+		rules: {
+			"@typescript-eslint/no-unused-vars": "warn",
+		},
+	},
 ];
 
 export default eslintConfig;
